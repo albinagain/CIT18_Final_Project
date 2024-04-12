1971fc06ebc53966090db33072277edc0a0f1a51
albin <ajmmosawan@gmail.com>
2024-04-12 13:38:15 +0800

    added profle and log out in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+@include('partials/header')
+@if (Route::has('login'))
+    <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10 flex space-x-4">
+        <x-responsive-nav-link :href="route('profile.edit')">
+            {{ __('Profile') }}
+        </x-responsive-nav-link>
+
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+
+            <x-responsive-nav-link :href="route('logout')"
+                    onclick="event.preventDefault();
+                                this.closest('form').submit();">
+                {{ __('Log Out') }}
+            </x-responsive-nav-link>
+        </form>
+    </div>
+@endif
+@include('partials/footer')
