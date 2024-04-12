7ca369140e69b849bfd3725f8acf9d64750e9f65
albin <ajmmosawan@gmail.com>
2024-04-12 12:37:37 +0800

    added login and register in landing view

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,3 +1,20 @@
 @include('partials/header')
-
+@if (Route::has('login'))
+    <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+        @auth
+            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                Dashboard
+            </a>
+        @else
+            <a href="{{ route('login') }}" class="font-semibold text-gray-900 hover:text-gray-600 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                Log in
+            </a>
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-900 hover:text-gray-600 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                    Register
+                </a>
+            @endif
+        @endauth
+    </div>
+@endif
 @include('partials/footer')
