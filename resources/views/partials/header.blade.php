5624043522880c8131250b98c47e26ec359a3064
albin <ajmmosawan@gmail.com>
2024-04-12 12:30:45 +0800

    edited header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <header>
+        <div class="h-16 bg-lime-500 flex items-center px-2">
+            <h1 class="antialiased text-3xl font-bold text-center">A totally legit plant store</h1>
+        </div>
+
+    </header>
