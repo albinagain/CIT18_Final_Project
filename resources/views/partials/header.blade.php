339f092815025c7d0e76c1775516135e462d5b9c
Chavez <nimbus.arc@outlook.com>
2024-04-16 23:03:22 +0800

    add css and design the view

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @vite('resources/css/app.css')
+
 </head>
 <body>
     <header>
-        <div class="h-16 bg-lime-500 flex items-center px-2">
-            <h1 class="antialiased text-3xl font-bold text-center hidden sm:block">A totally legit plant store</h1>
-        </div>
-
+        <h1 class="antialiased text-3xl font-bold text-center hidden sm:block">A totally legit plant store</h1>
     </header>
+
